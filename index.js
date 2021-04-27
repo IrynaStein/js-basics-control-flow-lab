7353e4c1f2b86453bbea34823d8e6061ad7a5a6b
Iryna Stein <isophiestein@gmail.com>
2021-04-26 20:25:53 -0600

    commit tree 5eb31e237e843736a92d23283774a291b5725507

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return ('This one is on me!');
+  } else if (distance >2000 && distance <2500){ //revisit this and add the && so it incorporates 2000 distance
+    return ('I will gladly take your thirty bucks.');
+} else if (distance >=2500){
+  return ('No can do.');
 }
+}
+scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
+
   // Write your code here!
+function ternaryCheckCity(cityName){
+  return (cityName === 'NYC') ? "Ok, sounds good.": "No go.";
 }
+ternaryCheckCity ('NYC');
+
+  // Write your code here!
+
+function switchOnCharmFromTip(tipMessage){
+  switch (tipMessage){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: 
+      return 'Bye.';
+  }
+};
